421dccf449502ce2a4bd39d4940d2db3b0a7515e
unplannedlunch <jonah.wolmark@gmail.com>
2021-04-27 19:20:02 -0400

    Styles

---
+++ src/styles/colors.ts
@@ -0,0 +1,11 @@
+export default {
+	primary: '#168aad',
+	secondary: '#52b69a',
+	tertiary: '#d9ed92',
+	white: '#FFF',
+	lighter: '#F3F3F3',
+	light: '#DAE1E7',
+	dark: '#444',
+	darker: '#222',
+	black: '#000',
+};
---
+++ src/styles/typography.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+	header: {
+		fontFamily: 'helvetica',
+		fontSize: 30,
+		fontStyle: 'normal',
+		fontWeight: 'normal',
+	},
+	body: {
+		fontFamily: 'helvetica',
+		fontSize: 20,
+		fontStyle: 'normal',
+		fontWeight: 'normal',
+	},
+});
