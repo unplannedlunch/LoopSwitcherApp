cb351fe84cdf16042f874b339af697e47782b672
unplannedlunch <jonah.wolmark@gmail.com>
2021-04-27 23:54:02 -0400

    fonts

--- src/styles/typography.ts
+++ src/styles/typography.ts
@@ -2,13 +2,13 @@ import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
 	header: {
-		fontFamily: 'helvetica',
-		fontSize: 30,
+		fontFamily: 'sans-serif-light',
+		fontSize: 25,
 		fontStyle: 'normal',
 		fontWeight: 'normal',
 	},
 	body: {
-		fontFamily: 'helvetica',
+		fontFamily: 'sans-serif-light',
 		fontSize: 20,
 		fontStyle: 'normal',
 		fontWeight: 'normal',
