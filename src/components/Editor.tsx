fa84fa7057d021d86b6234c1204d2960b14666dc
unplannedlunch <jonah.wolmark@gmail.com>
2021-04-27 23:54:54 -0400

    🦾

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,66 +1,82 @@
 import React, { ReactNode, useState } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View, ViewProps } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, TouchableOpacityProps, View, ViewProps } from 'react-native';
 import DraggableFlatList, { RenderItemParams } from 'react-native-draggable-flatlist';
 
-import { Colors } from '../styles';
+import { Colors, Typography } from '../styles';
+import { DragHandle } from '../assets/img';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
 const initialData = [
 	{
 		key: 'Loop01',
-		order: 1,
+		order: 0,
 		label: 'Loop 1',
 	},
 	{
 		key: 'Loop02',
-		order: 2,
+		order: 1,
 		label: 'Loop 2',
 	},
 	{
 		key: 'Loop03',
-		order: 3,
+		order: 2,
 		label: 'Loop 3',
 	},
 	{
 		key: 'Loop04',
-		order: 4,
+		order: 3,
 		label: 'Loop 4',
 	},
 	{
 		key: 'Loop05',
-		order: 5,
+		order: 4,
 		label: 'Loop 5',
 	},
 	{
 		key: 'Loop06',
-		order: 6,
+		order: 5,
 		label: 'Loop 6',
 	},
 	{
 		key: 'Loop07',
-		order: 7,
+		order: 6,
 		label: 'Loop 7',
 	},
 ];
 
+const renderItem: (params: RenderItemParams<{ key: string; order: number; label: string }>) => ReactNode = ({
+	item,
+	drag,
+	isActive,
+	index,
+}) => {
+
+	const DragButton = (props: TouchableOpacityProps) => {
+		return (
+			<TouchableOpacity /**onPress={drag}**/ style={props.style}>
+				<DragHandle />
+			</TouchableOpacity>
+		);
+	};
+
+	return (
+		<TouchableWithoutFeedback
+			style={[styles.loop, { backgroundColor: isActive ? Colors.tertiary : Colors.secondary }]}
+			onLongPress={drag}>
+			<Text style={{ flex: 1, textAlign: 'center', textAlignVertical: 'center', ...Typography.body }}>
+				{item.order}
+			</Text>
+			<Text style={{ flex: 4, textAlign: 'center', textAlignVertical: 'center', ...Typography.body }}>
+				{item.label + '\n' + item.key}
+			</Text>
+			<DragButton style={{ flex: 1, ...Typography.body }} />
+		</TouchableWithoutFeedback>
+	);
+};
+
 const Editor = (props: ViewProps) => {
 	const [data, setData] = useState(initialData);
 
-	const renderItem: (params: RenderItemParams<{ order: number; label: string }>) => ReactNode = ({
-		item,
-		drag,
-		isActive,
-		index,
-	}) => (
-		<TouchableOpacity
-			style={[styles.loop, { backgroundColor: isActive ? Colors.tertiary : Colors.secondary }]}
-			onLongPress={drag}
-			onPress={() => {
-				isActive = !isActive;
-				return true;
-			}}>
-			<Text style={{ flex: 1, textAlign: 'center', textAlignVertical: 'center' }}>{item?.label}</Text>
-		</TouchableOpacity>
-	);
 	return (
 		<View style={[props.style, styles.editor]}>
 			<View style={{ flex: 1 }}>
@@ -68,7 +84,14 @@ const Editor = (props: ViewProps) => {
 					data={data}
 					renderItem={renderItem}
 					keyExtractor={item => `draggable-item-${item.key}`}
-					onDragEnd={({ data: d }) => setData(d)}
+					onDragEnd={({ data: d }) => {
+						for (const i in d) {
+							d[i].order = parseInt(i, 10);
+						}
+						// eslint-disable-next-line no-alert
+						alert(JSON.stringify(d, null, 2));
+						setData(d);
+					}}
 				/>
 			</View>
 		</View>
@@ -83,12 +106,15 @@ const styles = StyleSheet.create({
 		alignItems: 'center',
 	},
 	loop: {
+		flexDirection: 'row',
 		elevation: 3,
-		width: 200,
-		height: 100,
+		width: 300,
+		height: 70,
 		margin: 10,
 		textAlignVertical: 'center',
 		textAlign: 'center',
+		borderRadius: 8,
+		borderWidth: 0,
 		shadowColor: Colors.black,
 		shadowOffset: { width: 100, height: 100 },
 		shadowOpacity: 1,
