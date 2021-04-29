f80e3c0a802823db0d33c24bf6b09e96ab7ba289
unplannedlunch <jonah.wolmark@gmail.com>
2021-04-29 19:15:37 -0400

    better config practices

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,11 @@
 module.exports = {
 	root: true,
-	extends: ['@react-native-community', 'plugin:prettier/recommended'],
+	plugins: ['prettier'],
 	rules: {
 		'prettier/prettier': 'warn',
 		'react-native/no-inline-styles': 'off',
 		'no-unused-vars': 'off',
 		'@typescript-eslint/no-unused-vars': ['warn'],
 	},
+	extends: ['@react-native-community', 'plugin:prettier/recommended'],
 };
