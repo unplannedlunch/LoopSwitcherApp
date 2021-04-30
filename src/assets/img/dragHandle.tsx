acdf0f96abf987af78623547383a781a0fd8b9fc
unplannedlunch <jonah.wolmark@gmail.com>
2021-04-30 01:54:31 -0400

    fixed color import

--- src/assets/img/dragHandle.tsx
+++ src/assets/img/dragHandle.tsx
@@ -1,7 +1,7 @@
 import Svg, { Rect } from 'react-native-svg';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { Colors } from '../../styles';
 
 export default () => (
 	<View style={[StyleSheet.absoluteFill, { alignItems: 'center', justifyContent: 'center' }]}>
